2030634f827b30268914273c9780245d370fff9c
salihmahduu9-cmyk <salihmahduu9@gmail.com>
2026-06-03 00:52:22 +0300

    Update watermark.lua

--- public/hercules/modules/watermark.lua
+++ public/hercules/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    return "--تم التشفير والحماية بواسطة SA | ALONE" .. code
 end
 
 return Watermark
